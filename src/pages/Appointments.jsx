7c8bd27dae3c2b25f3440ac03b92209d09e5aa2a
Cole <ccoombs002@drury.edu>
2025-07-17 07:50:59 -0500

    Added pictures for past works, updated minor css values to account for these pictures.

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -36,7 +36,7 @@ export default function Appointments() {
       }
     }
     fetchAppointments();
-  }, [selectedDate]);
+  }, [selectedDate, API_URL]);
 
   const handleBook = async (e) => {
     e.preventDefault();
